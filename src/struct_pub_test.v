689d1eec1361deb859a424948511e4c3e22c1f0c
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2024-11-08 22:37:26 +0100

    [ADD] Test `Problem.type` export

---
+++ src/struct_pub_test.v
@@ -0,0 +1,11 @@
+module problemdetail_struct_pub
+
+import net.urllib
+import problemdetail as pd
+
+fn test_type_empty() {
+	prob := pd.new(none)
+	assert prob.type.scheme == 'about'
+	assert prob.type.opaque == 'blank'
+	assert prob.type == urllib.parse('about:blank')!
+}
