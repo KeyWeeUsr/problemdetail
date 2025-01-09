244b7e336f32a6e34fa342e60fba10d8b1bb1bdb
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-01-09 21:01:27 +0100

    [ADD] Dereferencer for Windows and MacOS

---
+++ src/struct_win.v
@@ -0,0 +1,29 @@
+module problemdetail
+
+import os { Result }
+
+pub fn (prob Problem) visit_type() !Result {
+	dest := prob.type
+	if !prob.visit_allowed(dest.scheme) {
+		return DereferenceError{
+			url: prob.type.str()
+		}
+	}
+	return prob.dereferencer('start ${dest}')
+}
+
+pub fn (prob Problem) visit_instance() !Result {
+	dest := prob.instance
+	if dest.str() == '' {
+		return DereferenceError{
+			url: dest.str()
+		}
+	}
+
+	if !prob.visit_allowed(dest.scheme) {
+		return DereferenceError{
+			url: prob.type.str()
+		}
+	}
+	return prob.dereferencer('start ${dest}')
+}
